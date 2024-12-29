b4d0c96cb11ac83a9af4e350a88157a5f56430eb
olesien <linus-lindberg@outlook.com>
2024-12-29 02:44:34 +0100

    beep

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,9 +13,56 @@ import 'react-toastify/dist/ReactToastify.css'
 import { blue } from '@mui/material/colors';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,    // small phones
+            sm: 600,  // tablets
+            md: 960,  // small laptops
+            lg: 1280, // desktops
+            xl: 1920, // large screens
+        },
+    },
     palette: {
         primary: {
             main: blue[500],
+        },
+    },
+    typography: {
+        h1: {
+            fontSize: "4rem",
+            [`@media (max-width: ${960}px)`]: { // manually using the value of 'xs'
+                fontSize: "3rem",
+            },
+        },
+        h2: {
+            fontSize: "3rem",
+            [`@media (max-width: ${700}px)`]: {
+                fontSize: "2rem",
+            },
+        },
+        h3: {
+            fontSize: "2.2rem",
+            [`@media (max-width: ${700}px)`]: {
+                fontSize: "1.5rem",
+            },
+        },
+        h4: {
+            fontSize: "1.8rem",
+            [`@media (max-width: ${700}px)`]: {
+                fontSize: "1.2rem",
+            },
+        },
+        h5: {
+            fontSize: "1.4rem",
+            [`@media (max-width: ${700}px)`]: {
+                fontSize: "0.9rem",
+            },
+        },
+        h6: {
+            fontSize: "1rem",
+            [`@media (max-width: ${700}px)`]: {
+                fontSize: "0.7rem",
+            },
         },
     },
 });
