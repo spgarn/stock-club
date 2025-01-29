d01e5608083605dedf3f06d1eeff88f3547d6d44
olesien <linus-lindberg@outlook.com>
2025-01-29 13:26:03 +0100

    modal

--- frontend/src/pages/portfolio/components/ImportModal.tsx
+++ frontend/src/pages/portfolio/components/ImportModal.tsx
@@ -6,10 +6,24 @@ import useClubs from "../../../hooks/useClubs";
 import CSVParser, { CSVRow } from "../../../components/CSVParser";
 import LineMatcher, { Connection } from "../../../components/LineMatcher";
 import { useMemo, useState } from "react";
+import { StockHoldings } from "../../../api";
 
 const STATIC_KEYS = ["csv_import_date", "csv_import_transaction_type", "csv_import_price", "csv_import_quantity", "csv_import_ISIN", "csv_import_diff"]
+type Action = {
+    csv_import_date: Date;
+    csv_import_transaction_type: string;
+    csv_import_price: number;
+    csv_import_quantity: number;
+    csv_import_diff: string;
+};
+type AggregatedData = {
+    actions: Action[]
+    csv_import_ISIN: string;
+}
+
 export default function ImportModal({ handleClose, refetch }: { handleClose: () => void; refetch: () => void; }) {
     const { clubId } = useClubs();
+    // const [aggregatedData, setAggregatedData] = useState<AggregatedData[]>([]);
     const [table, setTable] = useState<{ keys: string[], values: string[], connections: Connection[], data: CSVRow[] }>({ keys: STATIC_KEYS, values: [], connections: [], data: [] })
     const { keys, values, connections, data } = table;
     console.log(connections);
@@ -20,7 +34,71 @@ export default function ImportModal({ handleClose, refetch }: { handleClose: () 
     }
 
     const interpretedData = useMemo(() => {
+        if (connections.length < 6) return [];
+        const aggregated = table.data.reduce((agg, row) => {
+            //Map the keys to the data
+            const [csv_import_date, csv_import_transaction_type, csv_import_price, csv_import_quantity, csv_import_ISIN, csv_import_diff] = STATIC_KEYS.map(key => {
+                const conn = table.connections.find(conn => conn.start === key);
+                if (conn === undefined) return null;
+                return row[conn.end]
+            })
+            if (!csv_import_date || !csv_import_transaction_type || !csv_import_price || !csv_import_quantity || !csv_import_ISIN || !csv_import_diff) return agg;
+            const existing = agg.findIndex(v => v.csv_import_ISIN == csv_import_ISIN);
+            if (existing < 0) {
+                //Add
+                agg.push({
+                    actions: [{
+                        csv_import_date: new Date(String(csv_import_date)),
+                        csv_import_transaction_type: String(csv_import_transaction_type),
+                        csv_import_price: Number(csv_import_price),
+                        csv_import_quantity: Number(csv_import_quantity),
+                        csv_import_diff: String(csv_import_diff)
+                    }],
+                    csv_import_ISIN: String(csv_import_ISIN),
+                })
+            } else {
+                const prev = agg[existing];
+                //Edit
+                agg.splice(existing, 1, {
+                    ...prev,
+                    actions: [
+                        ...prev.actions,
+                        {
+                            csv_import_date: new Date(String(csv_import_date)),
+                            csv_import_transaction_type: String(csv_import_transaction_type),
+                            csv_import_price: Number(csv_import_price),
+                            csv_import_quantity: Number(csv_import_quantity),
+                            csv_import_diff: String(csv_import_diff)
+                        }
+                    ]
+                })
+            }
+            return agg;
+        }, [] as AggregatedData[]);
 
+        const buys = [];
+        const sells = [];
+        const sumBy = (actions: Action[], type: string) => {
+            const { date, price, amount, count } = actions.reduce((prev, v) => {
+                if (v.csv_import_transaction_type === type) {
+                    //Add
+                    prev.count++;
+                    prev.price += v.csv_import_price;
+                    prev.amount += v.csv_import_quantity;
+                    if (v.csv_import_date > prev.date) {
+                        prev.date = v.csv_import_date;
+                    }
+                }
+                return prev;
+            }, { date: new Date(), price: 0, amount: 0, count: 0 })
+            return { date, price: price / count, amount: amount / count }
+        }
+        for (const data of aggregated) {
+            const buys = sumBy(data.actions, "Köp");
+            const sells = sumBy(data.actions, "Sälj");
+
+            //Take buys and try to "sell" as many as possible taking amount into account.
+        }
     }, [table])
 
     return (
